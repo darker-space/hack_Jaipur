bba36088b3292e611553ea540f3eeeaed112a492
Aakash <ak.grd.140@gmail.com>
2020-07-20 12:57:55 +0530

    ..

--- weCare/routes/user.js
+++ weCare/routes/user.js
@@ -7,6 +7,21 @@ var User = require("../models/user.js"),
     Hospital = require("../models/hospital.js"),
     Comment = require("../models/comment.js");
 
+router.get("/user/hospital", function(req, res) {
+    Hospital.find({}, function(err, hospitalArr) {
+        if (err) {
+            res.redirect("/")
+        } else {
+            res.render("hospital/yourHosp", { hospitalArr: hospitalArr });
+
+        }
+    })
+
+})
+
+router.get("/about", function(req, res) {
+    res.render("user/about.ejs");
+})
 
 
 module.exports = router;
